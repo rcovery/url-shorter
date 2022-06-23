602f41c9063550a97884e1fa043c0427ce1963cd
RCovery <ryanczpereira@gmail.com>
2022-06-22 22:36:06 -0300

    feat: create post url test

--- url-shorter/app/tests/test_views.py
+++ url-shorter/app/tests/test_views.py
@@ -4,7 +4,14 @@ class IndexTest(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_index_template(self):
+    def test_base_index_view(self):
         response = self.client.get('/')
 
+        self.assertTemplateUsed(response, 'app/index.html')
+
+    def test_post_index_template(self):
+        response = self.client.post('/', { 'url': 'https://teste.com' })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.context['short_url'], None)
         self.assertTemplateUsed(response, 'app/index.html')
