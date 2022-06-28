465c850d3708121e11e42b64579ed9e52fde602f
RCovery <ryanczpereira@gmail.com>
2022-06-27 22:09:37 -0300

    feat: url index added

--- url-shorter/src/models.py
+++ url-shorter/src/models.py
@@ -1,16 +1,45 @@
 from django.db import models
 
 class Users(models.Model):
-    email = models.CharField(max_length = 200, null = False, blank = False)
-    username = models.CharField(max_length = 30, null = False, blank = False)
-    password = models.CharField(max_length = 255, null = False, blank = False)
+    email = models.CharField(
+        max_length = 200,
+        null = False,
+        blank = False
+    )
+    username = models.CharField(
+        max_length = 30,
+        null = False,
+        blank = False
+    )
+    password = models.CharField(
+        max_length = 255,
+        null = False,
+        blank = False
+    )
 
     objects = models.Manager()
 
 class Urls(models.Model):
-    url = models.CharField(max_length = 200, null = False, blank = False)
-    name = models.CharField(max_length = 100, null = False, blank = False, primary_key = True)
-    user = models.ForeignKey(Users, on_delete = models.CASCADE, null = True, blank = True)
+    url = models.CharField(
+        max_length = 200,
+        null = False,
+        blank = False
+    )
+    name = models.CharField(
+        max_length = 100,
+        null = False,
+        blank = False,
+        primary_key = True,
+        db_index = True,
+        unique = True
+    )
+    user = models.ForeignKey(
+        Users,
+        on_delete = models.CASCADE,
+        null = True,
+        blank = True
+    )
+
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
