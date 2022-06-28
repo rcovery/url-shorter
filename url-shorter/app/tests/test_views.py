56285e3d4cc5c1446b3ddc13426bc44c108cf5fa
RCovery <ryanczpereira@gmail.com>
2022-06-27 22:09:50 -0300

    feat: post view created

--- url-shorter/app/tests/test_views.py
+++ url-shorter/app/tests/test_views.py
@@ -9,9 +9,25 @@ class IndexTest(TestCase):
 
         self.assertTemplateUsed(response, 'app/index.html')
 
-    def test_post_index_template(self):
+    def test_post_index(self):
         response = self.client.post('/', { 'url': 'https://teste.com' })
 
         self.assertEqual(response.status_code, 200)
         self.assertNotEqual(response.context['name'], None)
         self.assertTemplateUsed(response, 'app/index.html')
+
+    def test_post_index_with_name(self):
+        response = self.client.post('/', { 'url': 'https://teste.com', 'name': 'rcovery' })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.context['name'], None)
+        self.assertTemplateUsed(response, 'app/index.html')
+
+    def test_post_index_with_same_name(self):
+        self.client.post('/', { 'url': 'https://teste.com', 'name': 'rcovery' })
+
+        response = self.client.post('/', { 'url': 'https://teste2.com', 'name': 'rcovery' })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.context['name'], None)
+        self.assertTemplateUsed(response, 'app/404.html')
--- url-shorter/app/views.py
+++ url-shorter/app/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
 from .forms import UrlForm
+from src.models import Urls
 
 def index(request):
-    return render(request, 'app/index.html')
+    context = {}
+    template = 'app/index.html'
+    
+    if request.method == 'GET':
+        context['form'] = UrlForm()
+
+    elif request.method == 'POST':
+        context = dict(request.POST)
+
+        if 'name' in context:
+            # Check if exists
+            already_exists = Urls.objects.filter(
+                name = context['name']
+            )
+
+            if already_exists:
+                template = 'app/404.html'
+        else:
+            context['name'] = '8a3i93paldIAjw0'
+
+        form = UrlForm(context)
+        if form.is_valid():
+            new_url = Urls.objects.create(
+                name = context['name'],
+                url = context['url']
+            )
+
+    return render(request, template, context)
